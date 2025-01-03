15b272ce33594d566b56d9d39fcd2ef531a1357f
Regitze Nykjaer <reny0001@stud.kea.dk>
2025-01-03 19:42:00 +0100

    scroll js

--- menu.js
+++ menu.js
@@ -6,3 +6,49 @@ function burgerClick() {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
 }
+
+// scroll m. knapper
+
+// knap-start
+
+document.querySelector(".knap-start").addEventListener("click", function () {
+  const nowSection = document.querySelector(".knap-start").closest("section");
+  const nextSection = nowSection.nextElementSibling;
+
+  if (nextSection) {
+    nextSection.scrollIntoView();
+  }
+});
+
+// knap-web
+
+document.querySelector(".knap-web").addEventListener("click", function () {
+  const nowSection = document.querySelector(".knap-web").closest("section");
+  const nextSection = nowSection.nextElementSibling;
+
+  if (nextSection) {
+    nextSection.scrollIntoView();
+  }
+});
+
+// knap-uxui
+
+document.querySelector(".knap-uxui").addEventListener("click", function () {
+  const nowSection = document.querySelector(".knap-uxui").closest("section");
+  const nextSection = nowSection.nextElementSibling;
+
+  if (nextSection) {
+    nextSection.scrollIntoView();
+  }
+});
+
+// knap-ani
+
+document.querySelector(".knap-ani").addEventListener("click", function () {
+  const nowSection = document.querySelector(".knap-ani").closest("section");
+  const nextSection = nowSection.nextElementSibling;
+
+  if (nextSection) {
+    nextSection.scrollIntoView();
+  }
+});
